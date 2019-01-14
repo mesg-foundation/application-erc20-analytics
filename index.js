56a36a7a8e2086bf9617d394dc1b78d987f8cc7a
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-01-14 12:02:11 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const MESG = require('mesg-js').application()
+
+MESG.whenEvent({
+  serviceID: 'ethereum-erc20',
+  filter: 'transfer'
+}, {
+  serviceID: 'influxdb',
+  taskKey: 'write',
+  inputs: (_, eventData) => ({
+    measurement: "transfer",
+    tags: {
+      block: eventData.blockNumber,
+      address: eventData.contractAddress,
+      from: eventData.from,
+      to: eventData.to,
+    },
+    fields: {
+      value: parseFloat(eventData.value)
+    }
+  })
+})
